e101f11ec44e53fa6d2ff93f3bffa1583b4fbc60
Damien Lespiau <damien.lespiau@intel.com>
2011-07-22 14:05:14 +0100

    texture-tool: Add licence header

--- gdk-pixbuf-texture-tool.c
+++ gdk-pixbuf-texture-tool.c
@@ -1,3 +1,24 @@
+/*
+ * gdk-pixbuf-texture-tool - Play with texture files
+ *
+ * Copyright © 2011 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>
+ *
+ * Authors: Damien Lespiau <damien.lespiau@intel.com>
+ *
+ */
+
 #include <stdlib.h>
 #include <string.h>
 
